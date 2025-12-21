0f23ba47a0cd66d7e57e7277818b7b886976807d
danielishakutv <talk2ishakudaniel@gmail.com>
2025-12-21 10:03:46 +0200

    feat: enhanced registration form validation and location data
    - Make all fields mandatory except Comments and Referral Code
    - Set Learn2Earn program to default 'Yes' with required selection
    - Add comprehensive country list (195 countries)
    - Add all Nigerian states (36 states + FCT)
    - Add complete city lists for all Nigerian states
    - Change State label to 'State / Region' for clarity
    - Enforce State/Region and City as required for all countries
    - Auto-clear state/city fields when switching from Nigeria to other countries
    - Maintain dropdown functionality for Nigerian locations
    - Use text input for non-Nigerian locations

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -16,17 +16,245 @@ const hearOptions = [
 ]
 
 const countries = [
-  'Nigeria', 'United States', 'United Kingdom', 'Ghana', 'Kenya', 'Canada', 'India', 'South Africa'
+  'Nigeria',
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Andorra',
+  'Angola',
+  'Antigua and Barbuda',
+  'Argentina',
+  'Armenia',
+  'Australia',
+  'Austria',
+  'Azerbaijan',
+  'Bahamas',
+  'Bahrain',
+  'Bangladesh',
+  'Barbados',
+  'Belarus',
+  'Belgium',
+  'Belize',
+  'Benin',
+  'Bhutan',
+  'Bolivia',
+  'Bosnia and Herzegovina',
+  'Botswana',
+  'Brazil',
+  'Brunei',
+  'Bulgaria',
+  'Burkina Faso',
+  'Burundi',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Cape Verde',
+  'Central African Republic',
+  'Chad',
+  'Chile',
+  'China',
+  'Colombia',
+  'Comoros',
+  'Congo',
+  'Costa Rica',
+  'Croatia',
+  'Cuba',
+  'Cyprus',
+  'Czech Republic',
+  'Côte d\'Ivoire',
+  'Denmark',
+  'Djibouti',
+  'Dominica',
+  'Dominican Republic',
+  'Ecuador',
+  'Egypt',
+  'El Salvador',
+  'Equatorial Guinea',
+  'Eritrea',
+  'Estonia',
+  'Eswatini',
+  'Ethiopia',
+  'Fiji',
+  'Finland',
+  'France',
+  'Gabon',
+  'Gambia',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Grenada',
+  'Guatemala',
+  'Guinea',
+  'Guinea-Bissau',
+  'Guyana',
+  'Haiti',
+  'Honduras',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Israel',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Kiribati',
+  'Kosovo',
+  'Kuwait',
+  'Kyrgyzstan',
+  'Laos',
+  'Latvia',
+  'Lebanon',
+  'Lesotho',
+  'Liberia',
+  'Libya',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Madagascar',
+  'Malawi',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Marshall Islands',
+  'Mauritania',
+  'Mauritius',
+  'Mexico',
+  'Micronesia',
+  'Moldova',
+  'Monaco',
+  'Mongolia',
+  'Montenegro',
+  'Morocco',
+  'Mozambique',
+  'Myanmar',
+  'Namibia',
+  'Nauru',
+  'Nepal',
+  'Netherlands',
+  'New Zealand',
+  'Nicaragua',
+  'Niger',
+  'North Korea',
+  'North Macedonia',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Palau',
+  'Palestine',
+  'Panama',
+  'Papua New Guinea',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Rwanda',
+  'Saint Kitts and Nevis',
+  'Saint Lucia',
+  'Saint Vincent and the Grenadines',
+  'Samoa',
+  'San Marino',
+  'Sao Tome and Principe',
+  'Saudi Arabia',
+  'Senegal',
+  'Serbia',
+  'Seychelles',
+  'Sierra Leone',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Solomon Islands',
+  'Somalia',
+  'South Africa',
+  'South Korea',
+  'South Sudan',
+  'Spain',
+  'Sri Lanka',
+  'Sudan',
+  'Suriname',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Taiwan',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Timor-Leste',
+  'Togo',
+  'Tonga',
+  'Trinidad and Tobago',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Tuvalu',
+  'Uganda',
+  'Ukraine',
+  'United Arab Emirates',
+  'United Kingdom',
+  'United States',
+  'Uruguay',
+  'Uzbekistan',
+  'Vanuatu',
+  'Vatican City',
+  'Venezuela',
+  'Vietnam',
+  'Yemen',
+  'Zambia',
+  'Zimbabwe',
 ]
 
 const nigerianStates = [
   'Abia', 'Adamawa', 'Akwa Ibom', 'Anambra', 'Bauchi', 'Bayelsa', 'Benue', 'Borno', 'Cross River', 'Delta',
-  'Ebonyi', 'Edo', 'Ekiti', 'Enugu', 'FCT Abuja', 'Gombe', 'Imo', 'Jigawa', 'Kaduna', 'Kano',
+  'Ebonyi', 'Edo', 'Ekiti', 'Enugu', 'FCT Abuja', 'Gombe', 'Imo', 'Jigawa', 'Kaduna', 'Kano', 'Katsina', 'Kebbi', 'Lagos', 'Nasarawa', 'Niger', 'Ogun', 'Ondo', 'Osun', 'Oyo', 'Plateau', 'Rivers', 'Sokoto', 'Taraba', 'Yobe', 'Zamfara',
 ]
 
 const citiesByState = {
-  'Adamawa': ['Yola', 'Mubi', 'Girei', 'Ganye'],
-  'Lagos': ['Lagos', 'Ikeja', 'Lekki']
+  'Abia': ['Aba', 'Umuahia', 'Ohafia', 'Arochukwu', 'Bende', 'Ikot Ekpene', 'Orogu', 'Umunneochi', 'Isuikwuato', 'Isiala Ngwa', 'Other'],
+  'Adamawa': ['Yola', 'Mubi', 'Girei', 'Ganye', 'Numan', 'Hong', 'Demsa', 'Madagali', 'Maiha', 'Lamurde', 'Fufore', 'Jada', 'Toungo', 'Gombi', 'Song', 'Yamaltu-Deba', 'Michika', 'Mararaba', 'Gulak', 'Wuro-Birji', 'Gurin', 'Other'],
+  'Akwa Ibom': ['Uyo', 'Eket', 'Ikot Abasi', 'Oron', 'Ikot Ekpene', 'Abak', 'Etim Ekpo', 'Onna', 'Nsit Ibom', 'Other'],
+  'Anambra': ['Awka', 'Onitsha', 'Nnewi', 'Agulu', 'Ekwulobia', 'Ogidi', 'Idemili', 'Onitsha North', 'Nnewi South', 'Other'],
+  'Bauchi': ['Bauchi', 'Azare', 'Dass', 'Jada', 'Tafawa Balewa', 'Giade', 'Ningi', 'Katagum', 'Zaki', 'Other'],
+  'Bayelsa': ['Yenagoa', 'Sagbama', 'Brass', 'Nembe', 'Ekeremor', 'Ogbia', 'Southern Ijaw', 'Kolokuma-Opokuma', 'Other'],
+  'Benue': ['Makurdi', 'Otukpo', 'Gboko', 'Vandeikya', 'Katsina-Ala', 'Ukum', 'Oju', 'Ogbadibo', 'Tarka', 'Other'],
+  'Borno': ['Maiduguri', 'Biu', 'Damaturu', 'Gashua', 'Potiskum', 'Gujba', 'Jere', 'Chibok', 'Konduga', 'Other'],
+  'Cross River': ['Calabar', 'Ogoja', 'Ikom', 'Obudu', 'Bekwarra', 'Yala', 'Mamfe', 'Obubra', 'Akamkpa', 'Other'],
+  'Delta': ['Asaba', 'Warri', 'Sapele', 'Effurun', 'Agbor', 'Oleh', 'Uvwie', 'Isoko', 'Okpe', 'Other'],
+  'Ebonyi': ['Abakaliki', 'Onueke', 'Izzi', 'Afikpo', 'Ebonyi', 'Ohaozara', 'Ezza', 'Ishielu', 'Other'],
+  'Edo': ['Benin City', 'Auchi', 'Ekpoma', 'Ubiaja', 'Uromi', 'Irrua', 'Edo', 'Sapele', 'Uzebba', 'Other'],
+  'Ekiti': ['Ado Ekiti', 'Ikere', 'Ijero', 'Irepodun', 'Oye', 'Gbonyin', 'Ise-Orun', 'Moba', 'Emure', 'Other'],
+  'Enugu': ['Enugu', 'Nsukka', 'Agbani', 'Awgu', 'Oji River', 'Enugu Ezike', 'Udi', 'Ezeagu', 'Igbo-Eze', 'Other'],
+  'FCT Abuja': ['Abuja', 'Gwagwalada', 'Kuje', 'Kwali', 'Bwari', 'Abaji', 'Bwari', 'Kubwa', 'Other'],
+  'Gombe': ['Gombe', 'Bajoga', 'Balanga', 'Dukku', 'Funakaye', 'Kaltungo', 'Kwami', 'Yamaltu', 'Other'],
+  'Imo': ['Owerri', 'Mbaise', 'Orlu', 'Okigwe', 'Oguta', 'Isiala Mbano', 'Ikeduru', 'Ohaji-Egbema', 'Other'],
+  'Jigawa': ['Dutse', 'Kazaure', 'Gumel', 'Hadejia', 'Kafin Hausa', 'Babbar', 'Guri', 'Ringim', 'Sule Tankarkar', 'Other'],
+  'Kaduna': ['Kaduna', 'Zaria', 'Kafanchan', 'Samaru', 'Kachia', 'Ikeja', 'Kagarko', 'Kachia', 'Jaba', 'Other'],
+  'Kano': ['Kano', 'Kano City', 'Wudil', 'Karaye', 'Nassarawa', 'Gezawa', 'Gaya', 'Rano', 'Takai', 'Other'],
+  'Katsina': ['Katsina', 'Kankara', 'Daura', 'Funtua', 'Malumfashi', 'Rimi', 'Jibia', 'Batagarawa', 'Sabuwa', 'Other'],
+  'Kebbi': ['Birnin Kebbi', 'Argungu', 'Bunza', 'Jega', 'Yauri', 'Zuru', 'Maiyama', 'Bagudo', 'Suru', 'Other'],
+  'Lagos': ['Lagos', 'Ikeja', 'Lekki', 'Victoria Island', 'Surulere', 'Ajah', 'Ikorodu', 'Epe', 'Badagry', 'Ibeju-Lekki', 'Other'],
+  'Nasarawa': ['Lafia', 'Keffi', 'Nasarawa', 'Akwanga', 'Obi', 'Kagarko', 'Gwagwalada', 'Toto', 'Other'],
+  'Niger': ['Minna', 'Suleja', 'Bida', 'Kontagora', 'Lapai', 'Tegina', 'Paikoro', 'Rijau', 'Other'],
+  'Ogun': ['Abeokuta', 'Ijebu-Ode', 'Sagamu', 'Ifo', 'Ijebu-Igbo', 'Owu', 'Ado-Odo', 'Egbado', 'Other'],
+  'Ondo': ['Akure', 'Owo', 'Ose', 'Ondo City', 'Ijare', 'Ijebu-Owo', 'Oka', 'Ile-Oluji', 'Other'],
+  'Osun': ['Osogbo', 'Ilesha', 'Iwo', 'Ilesa', 'Ede', 'Ipetu-Ijesa', 'Ila', 'Ibokun', 'Other'],
+  'Oyo': ['Ibadan', 'Oyo', 'Ogbomoso', 'Iseyin', 'Saki', 'Ibarapa', 'Ajaawa', 'Orelope', 'Other'],
+  'Plateau': ['Jos', 'Bukuru', 'Pankshin', 'Mangu', 'Barkin Ladi', 'Bokkos', 'Shendam', 'Mikang', 'Other'],
+  'Rivers': ['Port Harcourt', 'Obio-Akpor', 'Eleme', 'Bonny', 'Okrika', 'Port Harcourt City', 'Opobo', 'Andoni', 'Other'],
+  'Sokoto': ['Sokoto', 'Gusau', 'Tambuwal', 'Gudu', 'Rabah', 'Wurno', 'Binji', 'Bodinga', 'Other'],
+  'Taraba': ['Jalingo', 'Takum', 'Gembu', 'Wukari', 'Zing', 'Gassol', 'Bali', 'Sardauna', 'Other'],
+  'Yobe': ['Damaturu', 'Potiskum', 'Gashua', 'Gujba', 'Karasuwa', 'Yunusari', 'Bade', 'Geidam', 'Other'],
+  'Zamfara': ['Gusau', 'Kaura Namoda', 'Maru', 'Bakura', 'Talata-Mafara', 'Anka', 'Tsafe', 'Bungudu', 'Other'],
 }
 
 function Registration() {
@@ -210,7 +438,7 @@ function Registration() {
                 </label>
                 <label className="field">
                   <span>Middle Name</span>
-                  <input id="1886087831" name="entry.1886087831" type="text" placeholder="Middle name (optional)" defaultValue={registrationPrefills['entry.1886087831'] || ''} />
+                  <input id="1886087831" name="entry.1886087831" type="text" placeholder="Middle name" required defaultValue={registrationPrefills['entry.1886087831'] || ''} />
                 </label>
               </div>
 
@@ -266,12 +494,22 @@ function Registration() {
                 <div className="field">
                   <span>Country</span>
                   <div className="custom-select-wrapper" ref={countryWrapperRef}>
-                    <input type="text" value={countrySearch || selectedCountry} onChange={(e) => { setCountrySearch(e.target.value); if (e.target.value && selectedCountry) { setSelectedCountry('') } setShowCountryDropdown(true) }} onFocus={() => { setCountrySearch(''); setShowCountryDropdown(true) }} placeholder="Type to search countries... (e.g., Nigeria)" className="country-search-input" />
+                    <input type="text" value={countrySearch || selectedCountry} onChange={(e) => { setCountrySearch(e.target.value); if (e.target.value && selectedCountry) { setSelectedCountry('') } setShowCountryDropdown(true) }} onFocus={() => { setCountrySearch(''); setShowCountryDropdown(true) }} placeholder="Type to search countries... (e.g., Nigeria)" className="country-search-input" required />
                     <input type="hidden" id="1837299617" name="entry.1837299617" value={selectedCountry} />
                     {showCountryDropdown && (
                       <div className="country-dropdown">
                         {countries.filter(c => c.toLowerCase().includes(countrySearch.toLowerCase())).map(country => (
-                          <div key={country} className="country-option" onClick={() => { setSelectedCountry(country); setCountrySearch(''); setShowCountryDropdown(false) }}>{country}</div>
+                          <div key={country} className="country-option" onClick={() => { 
+                            setSelectedCountry(country); 
+                            setCountrySearch(''); 
+                            setShowCountryDropdown(false);
+                            if (country !== 'Nigeria') {
+                              setSelectedState('');
+                              setSelectedCity('');
+                              setStateSearch('');
+                              setCitySearch('');
+                            }
+                          }}>{country}</div>
                         ))}
                       </div>
                     )}
@@ -282,9 +520,9 @@ function Registration() {
               <div className="field-row">
                 {selectedCountry === 'Nigeria' ? (
                   <div className="field">
-                    <span>State</span>
+                    <span>State / Region</span>
                     <div className="custom-select-wrapper" ref={stateWrapperRef}>
-                      <input type="text" value={stateSearch || selectedState} onChange={(e) => { setStateSearch(e.target.value); setShowStateDropdown(true); if (e.target.value) setSelectedState('') }} onFocus={() => setShowStateDropdown(true)} className="country-search-input" placeholder="Type to search states... (e.g., Lagos)" />
+                      <input type="text" value={stateSearch || selectedState} onChange={(e) => { setStateSearch(e.target.value); setShowStateDropdown(true); if (e.target.value) setSelectedState('') }} onFocus={() => setShowStateDropdown(true)} className="country-search-input" placeholder="Type to search states... (e.g., Lagos)" required />
                       <input type="hidden" id="1870628818" name="entry.1870628818" value={selectedState} />
                       {showStateDropdown && (
                         <div className="country-dropdown">
@@ -297,8 +535,8 @@ function Registration() {
                   </div>
                 ) : (
                   <label className="field">
-                    <span>State</span>
-                    <input id="1870628818" name="entry.1870628818" type="text" placeholder="State" defaultValue={registrationPrefills['entry.1870628818'] || ''} />
+                    <span>State / Region</span>
+                    <input id="1870628818" name="entry.1870628818" type="text" placeholder="State / Region" value={selectedState} onChange={(e) => setSelectedState(e.target.value)} required />
                   </label>
                 )}
 
@@ -306,7 +544,7 @@ function Registration() {
                   <div className="field">
                     <span>City</span>
                     <div className="custom-select-wrapper" ref={cityWrapperRef}>
-                      <input type="text" value={citySearch || selectedCity} onChange={(e) => { setCitySearch(e.target.value); setShowCityDropdown(true); if (e.target.value) setSelectedCity('') }} onFocus={() => setShowCityDropdown(true)} className="country-search-input" placeholder="Type to search cities..." />
+                      <input type="text" value={citySearch || selectedCity} onChange={(e) => { setCitySearch(e.target.value); setShowCityDropdown(true); if (e.target.value) setSelectedCity('') }} onFocus={() => setShowCityDropdown(true)} className="country-search-input" placeholder="Type to search cities..." required />
                       <input type="hidden" id="1603432109" name="entry.1603432109" value={selectedCity} />
                       {showCityDropdown && (
                         <div className="country-dropdown">
@@ -320,7 +558,7 @@ function Registration() {
                 ) : (
                   <label className="field">
                     <span>City</span>
-                    <input id="1603432109" name="entry.1603432109" type="text" placeholder="Lagos" defaultValue={registrationPrefills['entry.1603432109'] || ''} />
+                    <input id="1603432109" name="entry.1603432109" type="text" placeholder="City" value={selectedCity} onChange={(e) => setSelectedCity(e.target.value)} required />
                   </label>
                 )}
               </div>
@@ -355,7 +593,13 @@ function Registration() {
                 <div className="pill-group">
                   {['Yes', 'No'].map((option) => (
                     <label key={option} className="pill-option">
-                      <input type="radio" name="entry.2147449936" value={option} />
+                      <input
+                        type="radio"
+                        name="entry.2147449936"
+                        value={option}
+                        required
+                        defaultChecked={(registrationPrefills['entry.2147449936'] || 'Yes') === option}
+                      />
                       <span>{option}</span>
                     </label>
                   ))}
